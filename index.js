bc789be121c6ceeb90e751136227dfbf27a1e22d
mazhanbaig <mazhanbaig44@gmail.com>
2026-02-14 19:05:37 +0500

    cors err solved

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const dataRoutes = require("./routes/data");
 const imageRoutes = require("./routes/images");
 
 const app = express();
-app.use(cors())
-app.use(express.json());
+app.use(cors({
+    origin: "https://zstate.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"]
+})); app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
@@ -19,4 +21,3 @@ app.use("/api/images", imageRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
- 
